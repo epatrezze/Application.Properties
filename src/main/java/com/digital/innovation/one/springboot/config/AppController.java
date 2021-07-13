90483e1423bb7b7a5cf8595b6e467a1734086d1d
bannzp <evandro.patrezze@hotmail.com>
2021-07-13 13:58:28 -0300

    Initial commit

---
+++ src/main/java/com/digital/innovation/one/springboot/config/AppController.java
@@ -0,0 +1,18 @@
+package com.digital.innovation.one.springboot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Value("${app.message}")
+    private String appMessage;
+
+    @GetMapping("/")
+    public String appMessage() {
+        return appMessage;
+    }
+
+}
---
+++ src/test/java/com/digital/innovation/one/springboot/config/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.innovation.one.springboot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
